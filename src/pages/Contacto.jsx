3d8e6300c35499a04c53491aab47276b3cb9a797
WilliamsUreta <163853944+WilliamsUreta@users.noreply.github.com>
2025-10-11 03:13:16 -0300

    prt3

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,28 +1,19 @@
 import '../styles/Contacto.css';
-import { Validacion } from '../components/molecules/Validacion';
 
 function Contacto() {
-
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log({ username, email, message });
-    };
 
     return (
         <div className="text-center text-white" id='Contacto'>
             <h2 className="text-center"> Contacto </h2>
             <from>
                 <p>Nombre:</p>
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                
                 <p>Correo:</p>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                
                 <p>Mensaje:</p>
-                <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+                
             <button className="btn btn-primary">Enviar</button>
-            <Validacion />
+            
             </from>
         </div>
     );
