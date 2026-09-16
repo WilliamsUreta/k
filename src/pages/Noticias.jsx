c7729740e7315c27598c2aaeb7bd415acdcc02aa
WilliamsUreta <163853944+WilliamsUreta@users.noreply.github.com>
2026-09-16 01:30:29 -0300

    Change header from 'Proyectos' to 'Noticias'

--- src/pages/Noticias.jsx
+++ src/pages/Noticias.jsx
@@ -5,7 +5,7 @@ import NoticiaCard from "../components/organisms/NoticiasCard";
 function Noticias() {
     return (
        <Container className="text-left text-white">
-            <h2 className="text-center"> Proyectos </h2>
+            <h2 className="text-center"> Noticias </h2>
             <Row className="justify-content-center">
                 {noticias.map((noticias) => (
                     <NoticiaCard key={noticias.id} noticias={noticias}/>
